36502aec194b25db67fbad7477f6ac5d65c8f9ae
Yanglin Tao <taoyanglin2001@163.com>
2022-06-17 11:37:19 -0400

    add authors

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
-import pymsql.cursor
+import pymysql.cursor
 
 app = Flask(__name__)
 
@@ -64,110 +64,136 @@ def customer_login_auth():
 		return render_template('customer_login.html', error=error)
 
 #TODO: Authenticates staff login
+#Author: Tianzuo Liu
 @app.route('/staff_login_auth', methods=['GET', 'POST'])
 def staff_login_auth():
 	pass
 
 #TODO: Authenticates customer register
+#Author: Tianzuo Liu
 @app.route('/customer_register_auth', methods=['GET', 'POST'])
 def customer_register_auth():
 	pass
 
 #TODO: Authenticates staff register
+#Author: Tianzuo Liu
 @app.route('/staff_register_auth', methods=['GET', 'POST'])
 def staff_register_auth():
 	pass
 
 #TODO: Customer home
+#Author: Tianzuo Liu
 @app.route('customer_home')
 def customer_home():
 	pass
 
 #TODO: Customer searches for flights
+#Author: Tianzuo Liu
+'''
+Search for future flights (one way or round trip) based on source city/airport name, 
+destination city/airport name, dates (departure or return).
+'''
+# Input: departure airport, arrival airport, daparture date, arrival date
+# Output: flight_number, departure_airport, departure date, arrival date, arrival airport
 @app.route('customer_search_flights')
 def customer_search_flights():
 	pass
 
 #TODO: Customer purchases a ticket (from result of searching flight)
+#Author: Tianzuo Liu
 @app.route('customer_search_flights')
 def purchase_ticket():
 	pass
 
 #TODO: Customer cancels a trip
+#Author: Tianzuo Liu
 @app.route('cancel_trip')
 def cancel_trip():
 	pass
 
 #TODO: Customer gives rating and comment
+#Author: Yanglin Tao
 @app.route('customer_rating')
 def customer_rating():
 	pass
 
 #TODO: Customer tracks spending
+#Author: Yanglin Tao
 @app.route('track_spending')
 def track_spending():
 	pass
 
 #TODO: Customer logout
+#Author: Yanglin Tao
 @app.route('customer_logout')
 def customer_logout():
 	pass
 
 #TODO: Staff views flights
+#Author: Yanglin Tao
 @app.route('staff_view_flights')
 def staff_view_flights():
 	pass
 
 #TODO: Staff creates new flights
+#Author: Yanglin Tao
 @app.route('staff_create_flight')
 def create_flight():
 	pass
 
 #TODO: Staff changes status of the flight
+#Author: Yanglin Tao
 @app.route('change_status')
 def change_status():
 	pass
 
 #TODO: Staff adds new airplane in the system
+#Author: Yanglin Tao
 @app.route('add_airplane')
 def add_airplane():
 	pass
 
 #TODO: Staff adds new airport in the system
+#Author: Justin Li
 @app.route('add_airport')
 def add_airport():
 	pass
 
 #TODO: Staff views the rating and comments of the flight, along with the average rating
+#Author: Justin Li
 @app.route('view_rating')
 def view_rating():
 	pass
 
 #TODO: Staff views the most frequent customer
+#Author: Justin Li
 @app.route('frequent_customer')
 def frequent_customer():
 	pass
 
 #TODO: Staff views all flights of a customer
+#Author: Justin Li
 @app.route('view_customer_flights')
 def view_customer_flights():
 	pass
 
 #TODO: Staff views reports
+#Author: Justin Li
 @app.route('view_reports')
 def view_reports():
 	pass
 
 #TODO: Staff views earned revenue
+#Author: Justin Li
 @app.route('view_revenue')
 def view_revenue():
 	pass
 
 #TODO: Staff logout
+#Author: Justin Li
 @app.route('staff_logout')
 def staff_logout():
 	pass
-
 
 #Run the app on localhost port 5000
 if __name__ == "__main__":
